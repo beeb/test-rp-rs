5fc328f8fcfae256d35d3ee555eb4e8d09450019
Valentin Bersier <vbersier@gmail.com>
2023-04-19 20:40:14 +0200

    refactor: updated dependencies

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@
 #![feature(async_fn_in_trait)]
 #![allow(incomplete_features)]
 
-use core::convert::Infallible;
-
 use cyw43::NetDriver;
+use cyw43_pio::PioSpi;
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_net::{
@@ -14,14 +13,13 @@ use embassy_net::{
     tcp::client::{TcpClient, TcpClientState},
     Config, Stack, StackResources,
 };
-use embassy_rp::peripherals::{PIN_23, PIN_24, PIN_25, PIN_29};
 use embassy_rp::{
     clocks::RoscRng,
-    gpio::{Flex, Level, Output},
+    gpio::{Level, Output},
+    peripherals::{DMA_CH0, PIN_23, PIN_25},
+    pio::{Pio0, PioPeripheral, PioStateMachineInstance, Sm0},
 };
 use embassy_time::{Duration, Timer};
-use embedded_hal_1::spi::ErrorType;
-use embedded_hal_async::spi::{ExclusiveDevice, SpiBusFlush, SpiBusRead, SpiBusWrite};
 use embedded_nal_async::{heapless::String, AddrType, Dns, IpAddr, Ipv4Addr};
 use rand_core::RngCore;
 use reqwless::{
@@ -44,7 +42,7 @@ async fn wifi_task(
     runner: cyw43::Runner<
         'static,
         Output<'static, PIN_23>,
-        ExclusiveDevice<MySpi, Output<'static, PIN_25>>,
+        PioSpi<PIN_25, PioStateMachineInstance<Pio0, Sm0>, DMA_CH0>,
     >,
 ) -> ! {
     runner.run().await
@@ -67,26 +65,18 @@ async fn main(spawner: Spawner) {
 
     let pwr = Output::new(p.PIN_23, Level::Low);
     let cs = Output::new(p.PIN_25, Level::High);
-    let clk = Output::new(p.PIN_29, Level::Low);
-    let mut dio = Flex::new(p.PIN_24);
-    dio.set_low();
-    dio.set_as_output();
 
-    let bus = MySpi { clk, dio };
-    let spi = ExclusiveDevice::new(bus, cs);
+    let (_, sm, _, _, _) = p.PIO0.split();
+    let dma = p.DMA_CH0;
+    let spi = PioSpi::new(sm, cs, p.PIN_24, p.PIN_29, dma);
 
     let state = singleton!(cyw43::State::new());
     let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
-
     spawner.spawn(wifi_task(runner)).unwrap();
 
     control.init(clm).await;
     control
         .set_power_management(cyw43::PowerManagementMode::PowerSave)
-        .await;
-
-    control
-        .join_wpa2(env!("WIFI_NETWORK"), env!("WIFI_PASSWORD"))
         .await;
 
     let config = Config::Dhcp(Default::default());
@@ -103,7 +93,11 @@ async fn main(spawner: Spawner) {
         seed
     ));
 
-    unwrap!(spawner.spawn(net_task(stack)));
+    spawner.spawn(net_task(stack)).unwrap();
+
+    control
+        .join_wpa2(env!("WIFI_NETWORK"), env!("WIFI_PASSWORD"))
+        .await;
 
     info!("Network stack initialized");
 
@@ -172,79 +166,6 @@ async fn main(spawner: Spawner) {
             }
         }
         Timer::after(Duration::from_secs(10)).await;
-    }
-}
-
-struct MySpi {
-    /// SPI clock
-    clk: Output<'static, PIN_29>,
-
-    /// 4 signals, all in one!!
-    /// - SPI MISO
-    /// - SPI MOSI
-    /// - IRQ
-    /// - strap to set to gSPI mode on boot.
-    dio: Flex<'static, PIN_24>,
-}
-
-impl ErrorType for MySpi {
-    type Error = Infallible;
-}
-
-impl SpiBusFlush for MySpi {
-    async fn flush(&mut self) -> Result<(), Self::Error> {
-        Ok(())
-    }
-}
-
-impl SpiBusRead<u32> for MySpi {
-    async fn read(&mut self, words: &mut [u32]) -> Result<(), Self::Error> {
-        self.dio.set_as_input();
-        for word in words {
-            let mut w = 0;
-            for _ in 0..32 {
-                w <<= 1;
-
-                // rising edge, sample data
-                if self.dio.is_high() {
-                    w |= 0x01;
-                }
-                self.clk.set_high();
-
-                // falling edge
-                self.clk.set_low();
-            }
-            *word = w
-        }
-
-        Ok(())
-    }
-}
-
-impl SpiBusWrite<u32> for MySpi {
-    async fn write(&mut self, words: &[u32]) -> Result<(), Self::Error> {
-        self.dio.set_as_output();
-        for word in words {
-            let mut word = *word;
-            for _ in 0..32 {
-                // falling edge, setup data
-                self.clk.set_low();
-                if word & 0x8000_0000 == 0 {
-                    self.dio.set_low();
-                } else {
-                    self.dio.set_high();
-                }
-
-                // rising edge
-                self.clk.set_high();
-
-                word <<= 1;
-            }
-        }
-        self.clk.set_low();
-
-        self.dio.set_as_input();
-        Ok(())
     }
 }
 
